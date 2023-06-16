174eee5e848565e0842ca3d96d995b3b299af3aa
SWET <swetzadafiya18@gmail.com>
2023-06-16 17:50:46 +0530

    DONE

---
+++ SWET.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+clrscr();
+printf("My Name is Swet\n");
+printf("I am 16 year old\n");
+printf("I am studying in S.D. Jain Modern School");
+
+getch();
+}
